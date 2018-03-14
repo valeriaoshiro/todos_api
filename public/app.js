7025603e3f5afa36c63f31a6e69952603766812c
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-03-14 18:16:31 +0000

    Fetch all todos from our API and display

--- public/app.js
+++ public/app.js
@@ -1,1 +1,12 @@
-alert('hi from app.js');
+/* global $*/
+$(document).ready(function(){
+    $.getJSON('/api/todos')
+    .then(addTodos);
+});
+
+function addTodos(todos){
+    todos.forEach(function(todo){
+        var newTodo = $('<li class="task">'+todo.name+'</li>');
+        $('.list').append(newTodo);
+    });
+}
